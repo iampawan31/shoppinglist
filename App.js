2a7042dd265e4474b2637f219799b67e7fd8a7ea
Pawan Kumar <surpawan@gmail.com>
2020-12-10 17:02:44 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,55 @@
+import React, {useState} from 'react';
+import {View, StyleSheet, FlatList, Alert} from 'react-native';
+import {v4 as uuidv4} from 'uuid';
+
+import Header from './components/Header';
+import ListItem from './components/ListItem';
+import AddItem from './components/AddItem';
+
+const App = () => {
+  const [items, setItems] = useState([
+    {id: uuidv4(), text: 'Milk'},
+    {id: uuidv4(), text: 'Coke'},
+    {id: uuidv4(), text: 'Cheese'},
+    {id: uuidv4(), text: 'Chips'},
+    {id: uuidv4(), text: 'Eggs'},
+    {id: uuidv4(), text: 'Brown Bread'},
+  ]);
+
+  const deleteItem = (id) => {
+    setItems((prevItems) => {
+      return prevItems.filter((item) => item.id !== id);
+    });
+  };
+
+  const addItem = (text) => {
+    if (!text) {
+      Alert.alert('Error', 'Please enter an item', [{text: 'Ok'}]);
+    } else {
+      setItems((prevItems) => {
+        return [{id: uuidv4(), text}, ...prevItems];
+      });
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Header />
+      <AddItem addItem={addItem} />
+      <FlatList
+        data={items}
+        renderItem={({item}) => (
+          <ListItem item={item} deleteItem={deleteItem} />
+        )}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
